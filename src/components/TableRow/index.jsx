583fd5d61d8575c0814232eec964c08cc9db1d39
Filipe Alves <filipe.alvesdsouza@gmail.com>
2022-12-17 22:05:09 -0300

    feat: add TableRow component

---
+++ src/components/TableRow/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { Card, Tag } from '@blueprintjs/core';
+
+import styles from './styles.module.css';
+
+function Text({ width, children, bold }) {
+  const formatedWidth = width ? `${width}%` : 'auto';
+  const fontWeight = bold ? 500 : 400;
+
+  return (
+    <p style={{ width: formatedWidth, fontWeight }} className={styles.text}>
+      {children}
+    </p>
+  );
+}
+
+export default function TableRow({ data }) {
+
+  return (
+    <Card className={styles.container}>
+      <Text width={25} bold={true}>{data.title}</Text>
+
+      <div style={{ width: '6%', textAlign: 'center' }}>
+        <Tag intent={data.severity} />
+      </div>
+
+      <div style={{ width: '12%' }}>
+        <Tag className={styles.status}>{data.status}</Tag>
+      </div>
+
+      <Text width={15}>
+        {data.trader.name}
+        {data.trader.alerts > 0 && (
+          <Tag minimal={true} round={true} className={styles.tag}>
+            {`+${data.trader.alerts}`}
+          </Tag>
+        )}
+      </Text>
+      <Text width={15}>{data.counterparty}</Text>
+      <Text width={12}>{data.book}</Text>
+      <Text width={15}>{data.source}</Text>
+    </Card>
+  );
+}
